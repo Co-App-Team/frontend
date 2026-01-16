712e9f26aa1354d2e5778c19b7cfcfc2c02990b3
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2026-01-15 21:13:34 -0600

    Rename pipeline job and remove eslint error

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,6 @@ import { Button, Container, Row, Col } from 'react-bootstrap';
 function App() {
   const [count, setCount] = useState(0);
 
-  const test = () => {
-    // Test eslint: https://eslint.org/docs/latest/rules/no-compare-neg-zero
-    if (1 === -0) {
-      console.log('This will never happen');
-    }
-  };
-
   return (
     <>
       <h1>React Bootstrap Button</h1>
