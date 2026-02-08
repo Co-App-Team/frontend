560da945f65d103ea64e8aeb49980da3d360c8f5
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2026-02-08 11:27:06 -0600

    Improve page design

--- src/pages/ConfirmEmailPage.jsx
+++ src/pages/ConfirmEmailPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button, Form, Spinner } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -5,16 +6,24 @@ const ConfirmEmailPage = () => {
   const location = useLocation();
   const { email } = location.state || {};
 
+  const [showError, setShowError] = useState(false);
+
+  const onSubmit = () => {
+    setShowError(true);
+  };
+
+  const resendCode = () => {};
+
   // TODO: Are codes always the same length? If so, automatically send the request once the user has entered the correct number of characters
+  // TODO: Resend code flow
 
   return (
     <div
       className="p-4 border rounded d-flex flex-column align-items-center"
-      style={{ maxWidth: '19rem' }}>
+      style={{ maxWidth: '22rem' }}>
       <h2 className="mt-4 mb-0">Confirm your email</h2>
       <p className="text-center mt-4">
-        We sent a confirmation email to <strong>{email}</strong>. Please check your inbox and enter
-        the code from the email below.
+        Please check <strong>{email}</strong> enter the code we sent you below.
       </p>
 
       <Form>
@@ -24,15 +33,19 @@ const ConfirmEmailPage = () => {
           <Form.Control
             type="text"
             placeholder="Enter confirmation code"
+            isInvalid={showError}
             // onChange={onEmailChange}
             // disabled={isLoading}
           />
+          <Form.Control.Feedback type="invalid">
+            Invalid confirmation code. Please try again.
+          </Form.Control.Feedback>
         </Form.Group>
         <div className="d-grid">
           <Button
             variant="primary"
             type="button"
-            // onClick={submit}
+            onClick={onSubmit}
             // size="lg"
             // disabled={isLoading}
           >
@@ -45,8 +58,18 @@ const ConfirmEmailPage = () => {
           </Button>
         </div>
       </Form>
+      <p className="mt-4">
+        Don't see an email? Check your spam folder or{' '}
+        <Button
+          className="p-0 align-baseline"
+          variant="link"
+          onClick={resendCode}>
+          resend the code
+        </Button>
+        .
+      </p>
       {/* {error && <p className="text-danger mt-3">{error}</p>} */}
-      <p className="mt-3">
+      <p className="mt-0">
         Not your email? Try <Link to="/signup">signing up</Link> or{' '}
         <Link to="/login">logging in</Link> with your email.
       </p>
