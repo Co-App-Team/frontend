c8f140727af29e089ec5d98995c16208b88e7231
yomi-adt <nbarlaab@gmail.com>
2026-02-10 18:03:40 -0600

    Updated search to have a secondary "other matches"

--- src/pages/DemoPage.jsx
+++ src/pages/DemoPage.jsx
@@ -4,37 +4,69 @@ import { useEffect, useState } from 'react';
 
 const DemoPage = () => {
   const [companies, setCompanies] = useState([]);
-  const [filteredCompanies, setFilteredCompanies] = useState([]);
+  const [topFilteredCompanies, setTopFilteredCompanies] = useState([]);
+  const [otherFilteredCompanies, setOtherFilteredCompanies] = useState([]);
   useEffect(() => {
     async function loadCompanies() {
       const data = await getCompanies();
       setCompanies(data.companies);
-      setFilteredCompanies(data.companies);
+      setTopFilteredCompanies(data.companies);
     }
     loadCompanies();
   }, []);
 
   const updateSearch = (value) => {
-    const result = companies.filter((c) =>
+    const topFilter = companies.filter((c) =>
       c.companyName.toLowerCase().startsWith(value.toLowerCase()),
     );
-    setFilteredCompanies(result);
+    const otherFilters = companies.filter((c) =>
+      c.companyName.toLowerCase().includes(value.toLowerCase()),
+    );
+
+    setOtherFilteredCompanies(otherFilters);
+    if (value === '') {
+      setOtherFilteredCompanies([]);
+    }
+    setTopFilteredCompanies(topFilter);
   };
 
   return (
     <>
-      <h1>Demo page</h1>
-      <Searchbar handleSearch={updateSearch} />
-      {companies ? (
+      <h1 className="m-2 p-2">Demo page</h1>
+      <Searchbar
+        handleSearch={updateSearch}
+        className="m-2 p-2"
+      />
+
+      {topFilteredCompanies.length == companies.length ? (
+        <p>Companies</p>
+      ) : (
+        <p>Showing top search results</p>
+      )}
+      {topFilteredCompanies.length != 0 ? (
         <ul>
-          {filteredCompanies.map((company, index) => (
+          {topFilteredCompanies.map((company, index) => (
             <li key={company.companyId}>
               {index}: {company.companyName}
             </li>
           ))}
         </ul>
       ) : (
-        <p>No companies to show</p>
+        <p>No companies match</p>
+      )}
+      {otherFilteredCompanies.length != 0 ? (
+        <>
+          <p>Other search results</p>
+          <ul>
+            {otherFilteredCompanies.map((company, index) => (
+              <li key={company.companyId}>
+                {index}: {company.companyName}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <></>
       )}
     </>
   );
