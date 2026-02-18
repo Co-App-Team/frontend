6921f569f321fb001ea341ab1809548591695ebb
yomi-adt <nbarlaab@gmail.com>
2026-02-18 16:48:19 -0600

    removed palceholder text

--- src/components/rateMyCoop/CompanyReviewModal.jsx
+++ src/components/rateMyCoop/CompanyReviewModal.jsx
@@ -56,11 +56,8 @@ function CompanyReviewModal({ company, showModal, hideModal }) {
         </div>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
+        <h4>Reviews</h4>
+        <p></p>
         {currReviews ? (
           <>
             {currReviews.reviews.map((review, index) => (
