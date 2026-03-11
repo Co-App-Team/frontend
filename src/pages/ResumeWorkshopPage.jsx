2bb3ff429ab76044d975e02048bbc11dba0086d2
ACM02 <aidancmcleod@gmail.com>
2026-03-11 12:55:29 -0500

    Style '?' pill

--- src/pages/ResumeWorkshopPage.jsx
+++ src/pages/ResumeWorkshopPage.jsx
@@ -1,18 +1,21 @@
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
-import { Button, Col, Container, Dropdown, Form, Row } from 'react-bootstrap';
+import { Badge, Button, Col, Container, Dropdown, Form, Row } from 'react-bootstrap';
 
 const ResumeWorkshopPage = () => {
   const [message, setMessage] = useState('');
 
   return (
-    <Container className="mt-3">
+    // TODO: When sm or smaller no right margin
+    <Container
+      fluid="sm"
+      className="mt-3">
       <Row className="text-start align-bottom d-flex align-items-end my-1 py-1">
         <Col>
           <h2 className="m-0 p-0">Resume Workshop</h2>
         </Col>
-        <Col className="d-flex justify-content-end">
+        <Col className="d-flex justify-content-end align-items-center">
           <Dropdown
             align="end"
             className="me-1 mt-1">
@@ -30,7 +33,11 @@ const ResumeWorkshopPage = () => {
               Options
             </Dropdown.Menu>
           </Dropdown>
-          ?
+          <Badge
+            pill
+            bg="secondary">
+            ?
+          </Badge>
         </Col>
       </Row>
       <Row className="mt-4">
@@ -44,6 +51,7 @@ const ResumeWorkshopPage = () => {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 placeholder="Tell us how you want to improve your resume..."
+                style={{ resize: 'none' }}
               />
             </Form.Group>
           </Form>
