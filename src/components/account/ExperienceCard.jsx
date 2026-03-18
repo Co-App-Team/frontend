2b9a4ab1531bb6c5bfa2715ea2d3caa387282790
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2026-03-18 16:33:35 -0500

    Add spinners/placeholders for experiences

--- src/components/account/ExperienceCard.jsx
+++ src/components/account/ExperienceCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Placeholder, Row, Spinner } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencil, faPlus, faTools, faTrash } from '@fortawesome/free-solid-svg-icons';
 import useApi from '../../hooks/useApi';
@@ -105,62 +105,75 @@ const ExperienceCard = () => {
         </h5>
       </Card.Header>
       <Card.Body className="p-4">
-        {data?.experience &&
-          data.experience.map((experience, index) => {
-            return (
-              <Row
-                key={index}
-                className="mb-3">
-                <Col
-                  xs={12}
-                  md={8}
-                  lg={9}>
-                  <div className="text-start">
-                    {/* TODO: Inefficient */}
-                    <h4>
-                      {
-                        companies?.find((company) => company.companyId === experience.companyId)
-                          .companyName
-                      }
-                    </h4>
-                    <h5 className="text-muted">{experience.roleTitle}</h5>
-                    <div>{experience.roleDescription}</div>
-                    <div className="text-muted">
-                      {experience.startDate} to{' '}
-                      {experience.endDate ? experience.endDate : 'Present'}
-                    </div>
-                  </div>
-                </Col>
-                <Col
-                  xs={12}
-                  md={4}
-                  lg={3}
-                  className="d-flex justify-content-end align-items-center">
-                  <div className="d-flex">
-                    <Button
-                      className="me-2 d-flex align-items-center"
-                      onClick={() => onEditClick(experience)}>
-                      Edit
-                      <FontAwesomeIcon
-                        icon={faPencil}
-                        className="ms-2"
-                      />
-                    </Button>
-                    <Button
-                      variant="danger"
-                      className="d-flex align-items-center"
-                      onClick={() => handleDeleteExperience(experience)}>
-                      Delete
-                      <FontAwesomeIcon
-                        icon={faTrash}
-                        className="ms-2"
-                      />
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-            );
-          })}
+        {data ? (
+          <>
+            {data?.experience &&
+              data.experience.map((experience, index) => {
+                return (
+                  <Row
+                    key={index}
+                    className="mb-3">
+                    <Col
+                      xs={12}
+                      md={8}
+                      lg={9}>
+                      <div className="text-start">
+                        {/* TODO: Inefficient */}
+                        <h4>
+                          {companies ? (
+                            <>
+                              {
+                                companies?.find(
+                                  (company) => company.companyId === experience.companyId,
+                                ).companyName
+                              }
+                            </>
+                          ) : (
+                            <Placeholder xs={6} />
+                          )}
+                        </h4>
+                        <h5 className="text-muted">{experience.roleTitle}</h5>
+                        <div>{experience.roleDescription}</div>
+                        <div className="text-muted">
+                          {experience.startDate} to{' '}
+                          {experience.endDate ? experience.endDate : 'Present'}
+                        </div>
+                      </div>
+                    </Col>
+                    <Col
+                      xs={12}
+                      md={4}
+                      lg={3}
+                      className="d-flex justify-content-end align-items-center">
+                      <div className="d-flex">
+                        <Button
+                          className="me-2 d-flex align-items-center"
+                          onClick={() => onEditClick(experience)}>
+                          Edit
+                          <FontAwesomeIcon
+                            icon={faPencil}
+                            className="ms-2"
+                          />
+                        </Button>
+                        <Button
+                          variant="danger"
+                          className="d-flex align-items-center"
+                          onClick={() => handleDeleteExperience(experience)}>
+                          Delete
+                          <FontAwesomeIcon
+                            icon={faTrash}
+                            className="ms-2"
+                          />
+                        </Button>
+                      </div>
+                    </Col>
+                  </Row>
+                );
+              })}
+          </>
+        ) : (
+          <Spinner />
+        )}
 
         <Row>
           <Col>
@@ -176,103 +189,5 @@ const ExperienceCard = () => {
     </Card>
   );
 };
-
-// TODO: Move into a component
-// const ExperienceEditor = ({ experience, index }) => {
-//   return (
-//     <Row
-//       key={index}
-//       className="mb-3 text-start">
-//       <Col
-//         xs={12}
-//         md={8}
-//         lg={9}>
-//         <Form>
-//           <Form.Group
-//             className="mb-2"
-//             controlId={`company-${index}`}>
-//             <Form.Label>Company</Form.Label>
-//             <Form.Control
-//               type="text"
-//               defaultValue={experience.companyId}
-//               placeholder="Enter company"
-//             />
-//           </Form.Group>
-
-//           <Form.Group
-//             className="mb-2"
-//             controlId={`role-${index}`}>
-//             <Form.Label>Role Title</Form.Label>
-//             <Form.Control
-//               type="text"
-//               defaultValue={experience.roleTitle}
-//               placeholder="Enter role title"
-//             />
-//           </Form.Group>
-
-//           <Form.Group
-//             className="mb-2"
-//             controlId={`description-${index}`}>
-//             <Form.Label>Role Description</Form.Label>
-//             <Form.Control
-//               as="textarea"
-//               rows={3}
-//               defaultValue={experience.roleDescription}
-//               placeholder="Enter role description"
-//             />
-//           </Form.Group>
-
-//           <Row className="mb-2">
-//             <Col xs={6}>
-//               <Form.Group controlId={`startDate-${index}`}>
-//                 <Form.Label>Start Date</Form.Label>
-//                 <Form.Control
-//                   type="date"
-//                   defaultValue={experience.startDate}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col xs={6}>
-//               <Form.Group controlId={`endDate-${index}`}>
-//                 <Form.Label>End Date</Form.Label>
-//                 <Form.Control
-//                   type="date"
-//                   defaultValue={experience.endDate || ''}
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//         </Form>
-//       </Col>
-
-//       <Col
-//         xs={12}
-//         md={4}
-//         lg={3}
-//         className="d-flex justify-content-end align-items-center mt-2 mt-md-0">
-//         <div className="">
-//           <Button
-//             className="me-2 d-flex align-items-center"
-//             variant="success">
-//             Save
-//             <FontAwesomeIcon
-//               icon={faSave}
-//               className="ms-2"
-//             />
-//           </Button>
-//           <Button
-//             className="d-flex align-items-center"
-//             variant="secondary">
-//             Cancel
-//             <FontAwesomeIcon
-//               icon={faTimes}
-//               className="ms-2"
-//             />
-//           </Button>
-//         </div>
-//       </Col>
-//     </Row>
-//   );
-// };
 
 export default ExperienceCard;
